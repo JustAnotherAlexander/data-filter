9faf02878ff44c4ee2c7b7c3d3051d9c5c8ade90
Aliaksander Matweyankov <aleksandreliuz@gmail.com>
2021-06-24 21:54:17 +0300

    feat: change design to glassmorphism and add switcher for the case sensitive filtration

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,8 +8,20 @@ let lengthFilterButton = document.getElementById('lengthFilter');
 let substringFilterButton = document.getElementById('substringFilter');
 let queryInput = document.querySelector('.main__form__input');
 let queryResult = document.querySelector('.main__form__result');
+let switcher = document.querySelector('.header__wrapper__switcher');
 const url = './assets/data/data.json';
 let dataArr;
+let lowerCaseMode = false;
+
+//creating Event Listener for switcher to move the redd circle
+switcher.addEventListener('click', () => {
+    switcher.classList.toggle('switcher_on');
+    if (lowerCaseMode) {
+        lowerCaseMode = false;
+    } else {
+        lowerCaseMode = true;
+    }
+})
 
 // creating Event Listeners for buttons
 lengthFilterButton.addEventListener('click', (event) => { 
@@ -20,6 +32,9 @@ lengthFilterButton.addEventListener('click', (event) => {
 substringFilterButton.addEventListener('click', (event) => { 
     event.preventDefault();
     let queryText = queryInput.value;
+    if (lowerCaseMode) {
+        queryText = queryText.toLowerCase();
+    }
     queryResult.value = FilterBySubstring(dataArr, queryText);
 });
 
